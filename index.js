595eeacca6803238566d9b6ae54142f796452f0a
Can <63351166+fastuptime@users.noreply.github.com>
2024-06-15 15:10:35 +0300

    Update index.js
    <3

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ global.client = new Client({
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
   },
   authStrategy: new LocalAuth(),
+  webVersionCache: {
+    type: "remote",
+    remotePath:
+      "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+  },
 });
 
 client.on("qr", (qr) => {
